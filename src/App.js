6decdb1df7b9fad37fd12e366c9b0bee4ce107f8
derenkarabulak <derenkarabulak@gmail.com>
2022-09-05 15:48:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Checkbox, IconButton, Box, Typography, Toolbar, FormGroup, FormControlLabel } from "@mui/material";
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
+const App = () => {
+
+  const [password, setPassword] = useState('');
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+          <Box sx={{ flexGrow: 1}}>
+            <AppBar sx={{ bgcolor: '#9C2D41'}} position="static">
+              <Toolbar>
+                <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                >
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+                  Password Generator
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Box>
+        </div>
+        <div className="header">
+          <h3 className="header">What would you like your password to contain?</h3>
+        </div>
+        <div className="form-group">
+          <FormGroup>
+            <FormControlLabel sx={{ color: '#9C2D41' }} control={<Checkbox style={{ color: '#9C2D41'}} />} label="Numbers" />
+            <FormControlLabel sx={{ color: '#9C2D41' }} control={<Checkbox style={{ color: '#9C2D41'}} />} label="Uppercase Letters" />
+            <FormControlLabel sx={{ color: '#9C2D41' }} control={<Checkbox style={{ color: '#9C2D41'}} />} label="Lowercase Letters" />
+            <FormControlLabel sx={{ color: '#9C2D41' }} control={<Checkbox style={{ color: '#9C2D41'}} />} label="Characters" />
+          </FormGroup>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
